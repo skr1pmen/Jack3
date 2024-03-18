db49bab4ab44871ede39063d6fb87617e56dcd1f
skr1pmen <48603232+skr1pmen@users.noreply.github.com>
2024-03-18 12:40:46 +0400

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,32 @@
+TOKEN = ""
+URL = "https://rmk.stavedu.ru:8010/moodle/eioswork/timetable/watchstudent.php?year=2023&month=8&group="
+USERAGENT = {}
+SCHEDULE = {
+    "today": {
+        'day': None,
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+        7: None,
+    },
+    "tomorrow": {
+        'day': None,
+        1: None,
+        2: None,
+        3: None,
+        4: None,
+        5: None,
+        6: None,
+        7: None,
+    }
+}
+DATABASE = {
+    "HOST": "host",
+    "USERNAME": "username",
+    "PASSWORD": "password",
+    "BASENAME": "dbname"
+}
+ADMINS = [id_admins]
