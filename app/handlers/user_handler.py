d64d1f5ffb48d6cc0dfa6b39cb6f52adb03d1d72
skr1pmen <Kiberkot.v.roymenge@gmail.com>
2025-03-03 02:11:49 +0400

    Suggestion ideas added

--- app/handlers/user_handler.py
+++ app/handlers/user_handler.py
@@ -146,6 +146,7 @@ async def get_new_schedule(bot: Bot):
         for task_item, task in enumerate(tasks_finish):
             if task.status == 200:
                 schedule = await task.text()
+                print(schedule)
                 soup = BeautifulSoup(schedule, "html.parser")
 
                 i = 1
